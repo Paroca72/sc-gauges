55fd718e82a51dc42b0a9613d1d8c3c14c0f8fc6
paroca72 <caneuva72>
2017-07-04 21:13:12 +0700

    Start settings

--- app/src/main/java/com/sccomponents/gauges/demo/MainActivity.java
+++ app/src/main/java/com/sccomponents/gauges/demo/MainActivity.java
@@ -1,7 +1,12 @@
 package com.sccomponents.gauges.demo;
 
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.sccomponents.gauges.ScArcGauge;
+import com.sccomponents.gauges.ScGauge;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Find the components
+        final TextView counter = (TextView) MainActivity.this.findViewById(R.id.counter);
+        ScArcGauge gauge = (ScArcGauge) this.findViewById(R.id.gauge);
+
+        // Set the features stroke cap style to rounded
+        gauge.findFeature(ScArcGauge.BASE_IDENTIFIER)
+                .getPainter().setStrokeCap(Paint.Cap.ROUND);
+        gauge.findFeature(ScArcGauge.PROGRESS_IDENTIFIER)
+                .getPainter().setStrokeCap(Paint.Cap.ROUND);
+
+        // If you set the value from the xml that not produce an event so I will change the
+        // value from code.
+        gauge.setHighValue(60);
+
+        // Each time I will change the value I must write it inside the counter text.
+        gauge.setOnEventListener(new ScGauge.OnEventListener() {
+            @Override
+            public void onValueChange(float lowValue, float highValue) {
+                counter.setText((int) highValue + "%");
+            }
+        });
     }
 }
